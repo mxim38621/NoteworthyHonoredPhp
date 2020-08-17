5131696b3e15fe23b0faac95bd719027004593b8
watcharin inwichit <dmfristblood3@gmail.com>
2020-08-17 12:54:53 +0000

    lab3-3

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(void) {
-  printf("Hello World 2020\n");
+int main(void)
+{
+  float a, b;
+  printf("Enter A : ");
+  scanf("%f", &a);
+  printf("Enter B : ");
+  scanf("%f", &b);
+  printf("%0.2f * %0.2f = %0.2f", a, b, a*b);
   return 0;
 }
